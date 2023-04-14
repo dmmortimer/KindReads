563674e3f1ca68c66f76a3bf3d69d6b20bcbbf04
Danielle Mortimer <danielle.mortimer@hey.com>
2023-04-14 19:24:18 -0400

    Add comments how to fetch products via curl

--- getproducts.py
+++ getproducts.py
@@ -1,14 +1,16 @@
-# works from products.json in local directory
-# can get last 250 products from https://kindreads.com/products.json?limit=250
-# can't go higher than this without API
-# todo - download complete list via API
 import json
 
-# age tags, used with Kids tag
-# tags contained in collection tags (map tag -> collection tag) - if in first list, collection tag must be present
-# option: automatically add the collection tag
+# works from products.json in local directory
+fn = 'products-250.json'
 
-# tbd: can Kids also have Fiction or Non-Fiction? Yes. But Kids overrules when choosing a shelf.
+# can get last 250 products from https://kindreads.com/products.json?limit=250
+# can get first 250 (alphabetical) products from admin api like this on Windows
+# curl -H "X-Shopify-Access-Token: FILLMEIN" "https://friends-bookshop.myshopify.com/admin/api/2023-04/products.json?limit=250&fields=id,tags,title,created_at,variants" > products-250.json
+# or this on Linux
+# curl -H 'X-Shopify-Access-Token: FILLMEIN' https://friends-bookshop.myshopify.com/admin/api/2023-04/products.json?limit=5 > products-5.json
+
+# known tags, used in tag validation
+
 parent_tags = [
         'Fiction',
         'Kids',
@@ -17,7 +19,7 @@ parent_tags = [
 ]
 
 kids_age_tags = [
-        'Board Books: Ages 0-3',    # should get rid of this tag
+        'Board Books: Ages 0-3',
         'Board books: Ages 0-3',
         'board books: ages 0-3',
         'Middle Grade: Ages 8-12',
@@ -27,7 +29,7 @@ kids_age_tags = [
         'Teen'
 ]
 
-# map of tags that need an accompanying collection tag
+# tags that need an accompanying collection tag
 tags_in_collections = {
         'Fantasy':'Fantasy & Sci-Fi',
         'Science Fiction':'Fantasy & Sci-Fi',
@@ -54,7 +56,7 @@ known_tags = [
         'Autobiography',
         'Biography',
         'BIPOC',
-        'Board Books: Ages 0-3',    # should get rid of this tag
+        'Board Books: Ages 0-3',
         'Board books: Ages 0-3',
         'board books: ages 0-3',
         'Business',
@@ -96,59 +98,55 @@ known_tags = [
         'Middle Grade: Ages 8-12',
         'Middle grade: Ages 8-12',
         'Music',
-'Mystery',
-'Mystery and Thriller',
-'Non-fiction',
-'Non-Fiction',
-'Paranormal',
-'Parenting',
-'Philosophy',
-'Photography',
-'Picture books: Ages 3-8',
-'Picture Books: Ages 3-8',
-'Poetry',
-'Police',
-'Politics',
-'Politics and History',
-'Psychological Thriller',
-'Psychology',
-'Queer',
-'Religion',
-'Romance',
-'Science',
-'Science Fiction',
-#'Self help',
-'Self Help',
-'Self-Help',
-'Short Stories',
-'Sociology',
-'Spanish',
-'Sports',
-'Spy',
-'Staff Pick',
-'Summer',
-'Technology',
-'Teen',
-'Thriller',
-'Travel',
-'True Crime',
-'War',
-'Writing'
+        'Mystery',
+        'Mystery and Thriller',
+        'Non-fiction',
+        'Non-Fiction',
+        'Paranormal',
+        'Parenting',
+        'Philosophy',
+        'Photography',
+        'Picture books: Ages 3-8',
+        'Picture Books: Ages 3-8',
+        'Poetry',
+        'Police',
+        'Politics',
+        'Politics and History',
+        'Psychological Thriller',
+        'Psychology',
+        'Queer',
+        'Religion',
+        'Romance',
+        'Science',
+        'Science Fiction',
+        #'Self help',
+        'Self Help',
+        'Self-Help',
+        'Short Stories',
+        'Sociology',
+        'Spanish',
+        'Sports',
+        'Spy',
+        'Staff Pick',
+        'Summer',
+        'Technology',
+        'Teen',
+        'Thriller',
+        'Travel',
+        'True Crime',
+        'War',
+        'Writing'
 ]
-# todo make the compare case insensitive (by upper-casing before compare)
-# workaround: when there is a matching case, add it to the list above - helps make these visible
-
-# later - a report that spits out the parent tag and optional age tag and optional collection tag and then other tags in a lump
-# will be useful for the reconciliation
-# later - report listing books sold with their parent tag etc - same report, qty 0 or non-0 indicates sold
 
 # validates tags and prints error messages
 def validate_tags(product):
     isbn = product['id']
     tags = product['tags']
     title = product['title']
+    created_at = product['created_at']
+
     available = None
-    # public products.json works like this
+    # public products.json looks like this
     if 'available' in product['variants'][0]:
         available = product['variants'][0]['available']
     else:
@@ -165,7 +163,7 @@ def validate_tags(product):
 
     # Let's skip books not in inventory
     if not available:
-        #print('Skipping sold-out',title)
+        #print('Skipping sold-out',title,'created at',created_at)
         return
 
     # Rule: Must be a known tag
@@ -176,28 +174,27 @@ def validate_tags(product):
     # Rule: Must be exactly one parent tag
     n = len(set(tags).intersection(set(parent_tags)))
     if n == 0:
-        print('Error:',isbn,'missing parent tag, expecting one of',parent_tags, 'got',tags)
+        print('Error:',isbn,title,'missing parent tag, expecting one of',parent_tags, 'got',tags)
     elif n>1 and 'Kids' not in tags:
-        print('Error:',isbn,'has',n,'parent tags, for Adult expecting only one of',parent_tags,'got',tags)
+        print('Error:',isbn,title,'has',n,'parent tags, for Adult expecting only one of',parent_tags,'got',tags)
     elif n>2 and 'Kids' in tags:
-        print('Error:',isbn,'has',n,'parent tags, for Kids expecting at most one more of',parent_tags,'got',tags)
+        print('Error:',isbn,title,'has',n,'parent tags, for Kids expecting at most one more of',parent_tags,'got',tags)
 
     # Rule: For Kids, must be at most one age tag
     if 'Kids' in tags:
         n = len(set(tags).intersection(set(kids_age_tags)))
         # pot-pourri doesn't have an age group tag, so allow n == 0
         if n>1:
-            print('Error:',isbn,'has',n,'age group tags, expecting at most one of',kids_age_tags)
+            print('Error:',isbn,title,'has',n,'age group tags, expecting at most one of',kids_age_tags)
 
 
     # Rule: for the combo collections, need consistent tags
     for tag in tags:
         if tag in tags_in_collections:
             if tags_in_collections[tag] not in tags:
-                print('Error:',isbn,'has tag',tag,'but not the collection tag',tags_in_collections[tag])
+                print('Error:',isbn,title,'has tag',tag,'but not the collection tag',tags_in_collections[tag])
 
-fn = 'products-250.json'
-with open(fn) as f:
+with open(fn,encoding="utf-8") as f:
     j = json.loads(f.read())
     products = j['products']
     print('Reading from',fn)
